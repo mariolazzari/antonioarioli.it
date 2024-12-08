c7af57e3b1859332bcc6bd1b8a5e8d3ab6ad998c
Mario Lazzari <mario.lazzari@gmail.com>
2024-12-08 17:21:07 +0100

    feat: bio page

---
+++ components/Job/JobProps.ts
@@ -0,0 +1,5 @@
+export type JobProps = {
+  title: string;
+  text: string;
+  pdf?: string;
+};
---
+++ components/Job/index.tsx
@@ -0,0 +1,36 @@
+import { JobProps } from "./JobProps";
+import { GraduationCap, FileText, LinkIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export function Job({ title, text, pdf }: JobProps) {
+  const icon = pdf ? <FileText size={32} /> : <GraduationCap size={32} />;
+
+  return (
+    <Card className="w-64 h-64 border border-muted hover:border-primary bg-gradient-to-br from-muted to-background">
+      <CardHeader>
+        <CardTitle className="mx-auto">{icon}</CardTitle>
+        <CardDescription className="font-semibold mt-2 text-primary text-lg mx-auto">
+          {title}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{text}</p>
+      </CardContent>
+      <CardFooter>
+        {pdf && (
+          <Link href={pdf} target="_blank">
+            <LinkIcon />
+          </Link>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
