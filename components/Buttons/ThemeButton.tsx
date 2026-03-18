aed99867915abdfd4d3fe3c1780ca9aa7e67c7b5
Mario Lazzari <mario.lazzari@gmail.com>
2026-03-18 22:30:37 +0100

    feat: next 16.2.0

--- components/Buttons/ThemeButton.tsx
+++ components/Buttons/ThemeButton.tsx
@@ -2,6 +2,7 @@
 import { Moon, Sun, SunMoon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { MouseEventHandler } from "react";
 
 const items = [
   { value: "light", icon: <Sun size={16} /> },
@@ -12,22 +13,24 @@ const items = [
 export function ThemeButton() {
   const { setTheme, theme } = useTheme();
 
+  const onItemCLick: MouseEventHandler<HTMLButtonElement> = e =>
+    e.stopPropagation();
+
   return (
     <ToggleGroup
       className="mx-auto"
       type="single"
       size="sm"
       value={theme}
+      onValueChange={setTheme}
       variant="outline"
     >
       {items.map(({ value, icon }) => (
         <ToggleGroupItem
+          key={value}
           value={value}
           aria-label={value}
-          onClick={e => {
-            e.stopPropagation();
-            setTheme(value);
-          }}
+          onClick={onItemCLick}
         >
           {icon}
         </ToggleGroupItem>
