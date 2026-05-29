9d10b006c9b484fcb6dbcbf58ac4922d4d1071a3
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-29 09:00:07 +0200

    refact: go backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  poweredByHeader: false,
+  reactStrictMode: false,
   reactCompiler: true,
 };
 
